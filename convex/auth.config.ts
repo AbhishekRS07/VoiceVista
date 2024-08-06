31f44f698c7074db07e1436ea829dc4d39022c2c
Abhishek <abhiraj812000@gmail.com>
2024-08-06 23:10:29 +0530

    Clerk Auth implemented

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+      {
+        domain: "https://saving-crow-5.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
